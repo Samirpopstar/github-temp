49c32024d8bf071f48ca9878245863cf9fb2bf92
Samirpopstar <coolsamiracharya.jt@gmail.com>
2024-01-15 10:55:30 +0545

    change index

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = 5500;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(`Server started on port ${port}`);
