6f1a5fd41c4a720e30c39ebe2054e22c1bc62c48
Samirpopstar <coolsamiracharya.jt@gmail.com>
2024-01-15 11:37:16 +0545

    added about route and a router

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import express from "express";
+import getRouter from "./router.js";
 
 const app = express();
 const port = 5500;
+
+app.use("/hello", getRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello Worldsadasdasdasd");
 });
 
-app.get("/about", (req,res)=>{
-    
-})
+app.get("/about", (req, res) => {});
 
-app.listen(`Server started on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
